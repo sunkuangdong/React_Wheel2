f37fdcefd884a0c0f18abd4b5f15625abdac78d0
褚煦露 <chuxulu@lezhixing.com.cn>
2020-06-30 20:27:00 +0800

    配置tsx转义成js 完成

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,26 @@
+const path = require('path')
 module.exports = {
+    mode: 'production',
     // 程序的入口在哪里
     entry: {
         index: './lib/index.tsx'
     },
+    // 配置出口
+    output: {
+        // 出口路径
+        path: path.resolve(__dirname, 'dist'),
+        // 库的名字FUI
+        library: 'FUI',
+        // 库的格式
+        // umd兼容了三种定义方式,commonJS define script
+        // 但是并没有多NB,只是做了if else
+        libraryTarget: 'umd',
+    },
     // 编译tsx
-    modules: {
+    module: {
         rules: [{
             test: /\.tsx?$/,
+            // 如果用了ts文件,使用下面loader翻译成js
             loader: 'awesome-typescript-loader'
         }]
     }
