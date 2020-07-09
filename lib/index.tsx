fd2717044282e2ea1d9b7f1b53fdc7d509468a65
sunkuangdong <sunkuangdong@ele-cloud.com>
2020-07-09 17:23:24 +0800

    添加 CSS

--- lib/index.tsx
+++ lib/index.tsx
@@ -1,7 +1,10 @@
 import React from 'react'
 import ReactDom from 'react-dom'
 import Icon from './icon'
+
 ReactDom.render(
   <div>
     <Icon name='wechat'/>
+    <Icon name='al'/>
+    <Icon name='baidu'/>
   </div>, document.getElementById('root'))
