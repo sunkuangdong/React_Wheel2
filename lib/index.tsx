468d0741be7dce1f6f5f3f548bd53e0f70a585a0
sunkuangdong <sunkuangdong@ele-cloud.com>
2020-07-13 21:17:44 +0800

    发布 测试

--- lib/index.tsx
+++ lib/index.tsx
@@ -1,15 +1,3 @@
-import React from 'react'
-import ReactDom from 'react-dom'
-import Icon from './icon/icon'
+import Icon from './icon/icon';
 
-
-const fn: React.MouseEventHandler = (e) => {
-  console.log(e);
-  console.log(e.target);
-}
-ReactDom.render(
-  <div>
-    <Icon name='baidu'
-          onClick={fn}
-    />
-  </div>, document.getElementById('root'))
+export {Icon};
