88fee84de68badc596bddfa6c50ba203211ffb13
sunkuangdong <sunkuangdong@ele-cloud.com>
2020-07-13 17:56:55 +0800

    add jest-junit

--- jest.config.js
+++ jest.config.js
@@ -5,7 +5,7 @@ module.exports = {
   clearMocks: false,
 
   collectCoverage: true, // 是否收集测试覆盖率
-  reporters: ["default"], // jest-junit测试报表
+  reporters: ["default","jest-junit"], // jest-junit测试报表
   collectCoverageFrom: ["lib/**/*.{ts,tsx}", "!**/node_modules/**"],// 测试哪些代码
   coverageDirectory: 'coverage', // 生成的报告放在哪个目录里
   coverageReporters: ['text', 'lcov'], // 要生成哪些报告
