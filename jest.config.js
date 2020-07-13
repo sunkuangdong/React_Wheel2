95a3928fa661f9ad76965649b706ded40b2fa02f
sunkuangdong <sunkuangdong@ele-cloud.com>
2020-07-13 17:55:45 +0800

    add ci config

--- jest.config.js
+++ jest.config.js
@@ -1,30 +1,30 @@
 // https://jestjs.io/docs/en/configuration.html
 
 module.exports = {
-    verbose: true,
-    clearMocks: false,
+  verbose: true,
+  clearMocks: false,
 
-    collectCoverage: true, // 是否收集测试覆盖率
-    reporters: ["default"], // jest-junit测试报表
-    collectCoverageFrom: ["lib/**/*.{ts,tsx}", "!**/node_modules/**"],// 测试哪些代码
-    coverageDirectory: 'coverage', // 生成的报告放在哪个目录里
-    coverageReporters: ['text', 'lcov'], // 要生成哪些报告
+  collectCoverage: true, // 是否收集测试覆盖率
+  reporters: ["default"], // jest-junit测试报表
+  collectCoverageFrom: ["lib/**/*.{ts,tsx}", "!**/node_modules/**"],// 测试哪些代码
+  coverageDirectory: 'coverage', // 生成的报告放在哪个目录里
+  coverageReporters: ['text', 'lcov'], // 要生成哪些报告
 
-    moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx'],
-    moduleDirectories: ['node_modules'],
-    globals: {
-        'ts-jest': {
-            tsConfig: 'tsconfig.test.json',
-        },
+  moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx'],
+  moduleDirectories: ['node_modules'],
+  globals: {
+    'ts-jest': {
+      tsConfig: 'tsconfig.test.json',
     },
-    moduleNameMapper: {
-        "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/test/__mocks__/file-mock.js",
-        "\\.(css|less|sass|scss)$": "<rootDir>/test/__mocks__/object-mock.js",
-    },
-    testMatch: ['<rootDir>/**/__tests__/**/*.unit.(js|jsx|ts|tsx)'],
-    transform: {
-        "^.+unit\\.(js|jsx)$": "babel-jest",
-        '^.+\\.(ts|tsx)$': 'ts-jest',
-    },
-    setupFilesAfterEnv: ["<rootDir>test/setupTests.js"]
+  },
+  moduleNameMapper: {
+    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/test/__mocks__/file-mock.js",
+    "\\.(css|less|sass|scss)$": "<rootDir>/test/__mocks__/object-mock.js",
+  },
+  testMatch: ['<rootDir>/**/__tests__/**/*.unit.(js|jsx|ts|tsx)'],
+  transform: {
+    "^.+unit\\.(js|jsx)$": "babel-jest",
+    '^.+\\.(ts|tsx)$': 'ts-jest',
+  },
+  setupFilesAfterEnv: ["<rootDir>test/setupTests.js"]
 }
