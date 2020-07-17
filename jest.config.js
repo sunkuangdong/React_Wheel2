0833689c1b1a40441ddba957b55d4b9c921f5acc
sunkuangdong <sunkuangdong@ele-cloud.com>
2020-07-17 16:30:35 +0800

    add ci config

--- jest.config.js
+++ jest.config.js
@@ -3,12 +3,9 @@
 module.exports = {
   verbose: true,
   clearMocks: false,
+  reporters: ["default"], // jest-junit测试报表
 
-  collectCoverage: true, // 是否收集测试覆盖率
-  reporters: ["default","jest-junit"], // jest-junit测试报表
-  collectCoverageFrom: ["lib/**/*.{ts,tsx}", "!**/node_modules/**"],// 测试哪些代码
-  coverageDirectory: 'coverage', // 生成的报告放在哪个目录里
-  coverageReporters: ['text', 'lcov'], // 要生成哪些报告
+  collectCoverage: false, // 是否收集测试覆盖率
 
   moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx'],
   moduleDirectories: ['node_modules'],
