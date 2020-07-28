8b9008d2e340b34b63aafb17ba53c0c03cad5aa1
褚煦露 <chuxulu@lezhixing.com.cn>
2020-07-28 22:33:17 +0800

    footer样式

--- example.tsx
+++ example.tsx
@@ -1,26 +1,26 @@
-import React from "react";
-import ReactDom from "react-dom";
-import {HashRouter as Router, Route, Link} from "react-router-dom"
-import IconExample from "./lib/icon/icon.example";
-import ButtonExample from "./lib/button.example";
-import DialogExample from "./lib/dialog/dialog.example";
-import LayoutExample from "./lib/layout/layout.example";
-import {Aside, Footer, Header, Layout, Content} from "./lib/layout/layout";
-import "./example.scss"
+import React from 'react';
+import ReactDom from 'react-dom';
+import {HashRouter as Router, Route, Link} from 'react-router-dom';
+import IconExample from './lib/icon/icon.example';
+import ButtonExample from './lib/button.example';
+import DialogExample from './lib/dialog/dialog.example';
+import LayoutExample from './lib/layout/layout.example';
+import {Aside, Footer, Header, Layout, Content} from './lib/layout/layout';
+import './example.scss';
 
-const logo = require("./logo.png").default;
+const logo = require('./logo.png').default;
 
 ReactDom.render((
   <Router>
-    <Layout className={'page'}>
-      <Header className="head">
-        <div className="log">
-          <img src={logo} width="80px" height="80px"/>
-          SUI
+    <Layout className={'site-page'}>
+      <Header className="site-header">
+        <div className="logo">
+          <img src={logo} width="80px" height="80px" alt=""/>
+          <span>SUI</span>
         </div>
       </Header>
       <Layout>
-        <Aside>
+        <Aside className={"site-aside"}>
           <h2>组件</h2>
           <ul>
             <li>
@@ -44,9 +44,9 @@ ReactDom.render((
           <Route path="/layout" component={LayoutExample}/>
         </Content>
       </Layout>
-      <Footer>
-        footer
+      <Footer className={"site-footer"}>
+        &copy; 孙匡东
       </Footer>
     </Layout>
   </Router>
-), document.querySelector("#root"))
+), document.querySelector('#root'));
