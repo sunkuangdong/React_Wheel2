3ef443c2f241fef23daecb93ea99baaddc02e722
sunkuangdong <sunkuangdong@ele-cloud.com>
2020-08-03 14:41:58 +0800

    remove all files

--- README.md
+++
@@ -1,5 +0,0 @@
-# React_Wheel
-# 这个一个React轮子
-# 作者: 孙匡东
-
-[![CircleCI](https://circleci.com/gh/sunkuangdong/React_Wheel2.svg?style=svg)](https://circleci.com/gh/sunkuangdong/React_Wheel2)
--- jest.config.ci.js
+++
@@ -1,8 +0,0 @@
-const base = require('./jest.config')
-module.exports = Object.assign({}, base, {
-  collectCoverage: true, // 是否收集测试覆盖率
-  reporters: ["jest-junit"], // jest-junit测试报表
-  collectCoverageFrom: ["lib/**/*.{ts,tsx}", "!**/node_modules/**"],// 测试哪些代码
-  coverageDirectory: 'coverage', // 生成的报告放在哪个目录里
-  coverageReporters: ['text', 'lcov'], // 要生成哪些报告
-})
