a80112c4bd011bef4ac0092236feb762d3240f06
褚煦露 <chuxulu@lezhixing.com.cn>
2020-08-04 20:01:21 +0800

    表单样式 未完成

--- lib/form/form.example.tsx
+++ lib/form/form.example.tsx
@@ -1,16 +1,28 @@
 import React from 'react';
-import Form from './form';
+import Form, {FormValue} from './form';
 import {useState, Fragment} from 'react';
+import Validator from './validator';
 
 const FormExample: React.FunctionComponent = () => {
-  const [formData] = useState({
+  const [formData, setFormData] = useState<FormValue>({
     username: '',
     password: ''
   });
+  const [errors, setErrors] = useState({});
   const [fields] = useState([
     {name: 'username', label: '用户名', input: {type: 'text'}},
     {name: 'password', label: '密码', input: {type: 'password'}},
   ]);
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    const rules = [
+      {key: 'username', required: true},
+      {key: 'password', required: true},
+      {key: 'username', minLength: 8, maxLength: 16},
+      {key: 'username', pattern: /^[A-Za-z0-9]+$/},
+    ];
+    const errors = Validator(formData, rules);
+    setErrors(errors);
+  };
   return (
     <div>
       <Form value={formData} fields={fields}
@@ -20,6 +32,9 @@ const FormExample: React.FunctionComponent = () => {
                 <button>返回</button>
               </Fragment>
             }
+            onChange={(value) => setFormData(value)}
+            onSubmit={onSubmit}
+            errors={errors}
       />
     </div>
   );
--- lib/form/form.tsx
+++ lib/form/form.tsx
@@ -1,20 +1,38 @@
 import * as React from 'react';
 import {ReactFragment} from 'react';
 
+export interface FormValue {
+  [k: string]: string;
+}
+
 interface Props {
-  value: { [k: string]: string };
+  value: FormValue;
   fields: Array<{ name: string, label: string, input: { type: string } }>,
   buttons: ReactFragment;
-  onSubmit: React.FormEventHandler;
+  onSubmit: React.FormEventHandler<HTMLFormElement>;
+  onChange: (value: FormValue) => void;
+  errors: { [key: string]: string[] }
 }
 
 const Form: React.FunctionComponent<Props> = (props) => {
+  const formData = props.value;
+  const onSubmit: React.FormEventHandler<HTMLFormElement> = (e) => {
+    e.preventDefault();
+    props.onSubmit(e);
+  };
+  const onInputChange = (name: string, value: string) => {
+    const newFormValue = {...formData, [name]: value};
+    props.onChange(newFormValue);
+  };
   return (
-    <form>
+    <form onSubmit={onSubmit}>
       {props.fields.map(item =>
         <div key={item.name}>
           {item.label}
-          <input type={item.input.type}/>
+          <input type={item.input.type} value={formData[item.name]}
+                 onChange={(e) => onInputChange(item.name, e.target.value)}
+          />
+          <div>{props.errors[item.name]}</div>
         </div>
       )}
       <div>
