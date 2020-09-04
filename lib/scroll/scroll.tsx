043c14e6df3f07f84a7fe978a4d22a307f36f16e
sunkuangdong <sunkuangdong@ele-cloud.com>
2020-09-04 16:08:52 +0800

    JS 隐藏滚动条

--- lib/scroll/scroll.tsx
+++ lib/scroll/scroll.tsx
@@ -2,6 +2,7 @@ import * as React from "react";
 import {HTMLAttributes} from "react";
 import classes from "../helpers/classes";
 import "./scroll.scss"
+import scrollbarWidth from "./scroll_whith";
 
 interface Props extends HTMLAttributes<HTMLDivElement> {
 }
@@ -10,7 +11,7 @@ const Scroll: React.FunctionComponent<Props> = (props) => {
     const {children, ...rest} = props
     return (
         <div {...rest} className={classes("sun-scroll")}>
-           <div className={classes("sun-scroll-inner")}>
+           <div className={classes("sun-scroll-inner")} style={{right:-scrollbarWidth()}}>
                {children}
            </div>
         </div>
