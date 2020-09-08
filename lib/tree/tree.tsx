6f604167c42f99dbaafe4184a0b00bd6684a10f3
sunkuangdong <sunkuangdong@ele-cloud.com>
2020-09-08 09:53:43 +0800

    折叠功能

--- lib/tree/tree.tsx
+++ lib/tree/tree.tsx
@@ -10,57 +10,61 @@ const scopedClass = scopedClassMaker('sun-tree');
 const sc = scopedClass;
 const Tree: React.FunctionComponent<Props> = (props) => {
 // 递归渲染的函数
-  const renderItem = (
-    item: SourceDataItem,
-    level = 1) => {
-    const classes = {
-      ['level-' + level]: true,
-      'item': true
+    const renderItem = (
+        item: SourceDataItem,
+        level = 1) => {
+        const classes = {
+            ['level-' + level]: true,
+            'item': true
+        };
+        const checkout = props.multiple ?
+            props.selected.indexOf(item.value) >= 0 :
+            props.selected === item.value;
+        const onChange: ChangeEventHandler<HTMLInputElement> = (e) => {
+            if (props.multiple) {
+                if (e.target.checked) {
+                    props.onChange([...props.selected, item.value]);
+                } else {
+                    props.onChange(props.selected.filter(value => value !== item.value));
+                }
+            } else {
+                props.onChange(item.value);
+            }
+        };
+        const expand = () => {
+            setExpanded(true)
+        };
+        const collapse = () => {
+            setExpanded(false)
+        };
+        const [expanded, setExpanded] = useState(true);
+        return (
+            <div key={item.value} className={sc(classes)}>
+                <label className={sc('text')}>
+                    <input type="checkbox"
+                           onChange={onChange}
+                           checked={checkout}/>
+                    {item.text}
+                    {item.children &&
+                    <span onSelect={e => e.preventDefault()}>
+                        {expanded ?
+                            <span onClick={collapse}>-</span> :
+                            <span onClick={expand}>+</span>}
+                    </span>
+                    }
+                </label>
+                <div className={sc({children: true, collapsed: !expanded})}>
+                    {item.children?.map((child) => renderItem(child, level + 1))}
+                </div>
+            </div>
+        );
     };
-    const checkout = props.multiple ?
-      props.selected.indexOf(item.value) >= 0 :
-      props.selected === item.value;
-    const onChange: ChangeEventHandler<HTMLInputElement> = (e) => {
-      if (props.multiple) {
-        if (e.target.checked) {
-          props.onChange([...props.selected, item.value]);
-        } else {
-          props.onChange(props.selected.filter(value => value !== item.value));
-        }
-      } else {
-        props.onChange(item.value);
-      }
-    };
-    const expand = () => {};
-    const collapse = () => {};
-    const [expanded] = useState(true);
-    return (
-      <div key={item.value} className={sc(classes)}>
-        <label className={sc('text')}>
-          <input type="checkbox"
-                 onChange={onChange}
-                 checked={checkout}/>
-          {item.text}
-          {item.children &&
-          <span>
-            {expanded ?
-              <span onClick={expand}>+</span> :
-              <span onClick={collapse}>-</span>}
-          </span>
-          }
-        </label>
-        <div className={sc('children')}>
-          {item.children?.map((child) => renderItem(child, level + 1))}
-        </div>
-      </div>
-    );
-  };
 
-  const {children, onChange, ...rest} = props;
-  return (
-    <div {...rest}>
-      {props.sourceData?.map(item => renderItem(item))}
-    </div>
-  );
+    const {children, onChange, ...rest} = props;
+    return (
+        <div {...rest}>
+            {props.sourceData?.map(item => renderItem(item))}
+        </div>
+    );
 };
 export default Tree;
