7e802a649e635452106258fcbbf79d1f3f6f1d7d
sunkuangdong <sunkuangdong@ele-cloud.com>
2020-09-18 17:32:48 +0800

    button参数配置修改

--- site/configuration/ButttonDocument.tsx
+++ site/configuration/ButttonDocument.tsx
@@ -11,6 +11,7 @@ const ButtonDocument = () => {
                         <th>参数</th>
                         <th className="des">说明</th>
                         <th className="type">类型</th>
+                        <th className="type">可选值</th>
                         <th>默认值</th>
                     </tr>
                 </thead>
@@ -19,25 +20,29 @@ const ButtonDocument = () => {
                         <td>level</td>
                         <td className="des">不同样式风格的按钮</td>
                         <td className="type">string</td>
-                        <td>——</td>
+                        <td className="type">important | warning | normal | delete</td>
+                        <td>normal</td>
                     </tr>
                     <tr>
                         <td>name</td>
                         <td className="des">内嵌 Icon 图标的 name 属性</td>
                         <td className="type">string</td>
-                        <td>normal</td>
+                        <td className="type">——</td>
+                        <td>——</td>
                     </tr>
                     <tr>
                         <td>className</td>
                         <td className="des">自定义 Button 类名</td>
                         <td className="type">string</td>
-                        <td>normal</td>
+                        <td className="type"></td>
+                        <td>——</td>
                     </tr>
                     <tr>
                         <td>style</td>
                         <td className="des">自定义 Button 样式</td>
                         <td className="type">React.CSSProperties</td>
-                        <td>normal</td>
+                        <td className="type"></td>
+                        <td>——</td>
                     </tr>
                 </tbody>
             </table>
